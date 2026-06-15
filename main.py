d4879e985d7a1da9d3fc1139fe1937e6f09fe265
Ifsaurabh <saurabhsagar.official@gmail.com>
2026-06-15 15:22:25 +0530

    Add error handling and basic auth to FastAPI endpoint

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+
+
 # imports
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import TypedDict
 import uvicorn
 from agent.graph import app as aga
+import anthropic
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# security
+security = HTTPBasic()
+correct_username = os.getenv("BASIC_AUTH_USERNAME")
+correct_password = os.getenv("BASIC_AUTH_PASSWORD")
 
 # App
 app = FastAPI(title="Autonomous Humanitarian Report Generator")
@@ -17,8 +32,21 @@ class QueryResponse(BaseModel):
     
 
 @app.post("/question",  response_model=QueryResponse)
-def get_report(request: QueryRequest):
-    final_report = aga.invoke({"question": request.question})
+def get_report(request: QueryRequest, credentials: HTTPBasicCredentials = Depends(security)):
+    
+    if not secrets.compare_digest(credentials.username, correct_username) or \
+        not secrets.compare_digest(credentials.password, correct_password):
+            raise HTTPException(status_code=401, detail = "Invalid credentials")
+    
+    try:
+        final_report = aga.invoke({"question": request.question})
+    except anthropic.APIError as ae:
+        raise HTTPException(status_code=500, detail=f"Anthropic API error: {ae}")
+    except KeyError as ke:
+        raise HTTPException(status_code=500, detail=f"Report generation failed: {ke}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {e}")
+        
     return {"answer": final_report['final_report']}
 
 if __name__== "__main__":
